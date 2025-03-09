a3d638f18aea41292d8e69ec41fb888427604bc0
Bernardo Goes <bernardo.goes@infobase.com.br>
2025-03-09 12:08:07 -0300

    commitando

---
+++ src/main/java/com/goesbernardo/api_football/ApiFootballApplication.java
@@ -0,0 +1,13 @@
+package com.goesbernardo.api_football;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFootballApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFootballApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goesbernardo/api_football/ApiFootballApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goesbernardo.api_football;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFootballApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
